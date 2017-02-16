dbe79e7a16c38aaa2c5da8264610008ce6956f0c
Melvin <melvin0719@hotmail.com>
2017-02-16 18:41:12 +0800

    Refactor test

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -1,11 +1,15 @@
 package seedu.addressbook.data.person;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
 
 public class NameTest {
 
     @Test
-    public void isSimilar_sameName_returnsTrue() {
+    public void isSimilar_sameName_returnsTrue() throws IllegalValueException {
         Name name1 = new Name("Alice");
         Name name2 = new Name("Alice");
         assertTrue(name1.isSimilar(name2));
